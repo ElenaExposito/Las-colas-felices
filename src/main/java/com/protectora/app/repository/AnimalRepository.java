5ab2ad85ec74dac90408f19803769efd51058017
Marisa <marisaorlu@gmail.com>
2026-02-20 15:51:47 +0100

    Resolución Issue #10. Consultas Avanzadas y CRUD SQL

--- src/main/java/com/protectora/app/repository/AnimalRepository.java
+++ src/main/java/com/protectora/app/repository/AnimalRepository.java
@@ -4,15 +4,13 @@ import com.protectora.app.model.Animal;
 import com.protectora.app.model.Enums;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
-    // Consulta avanzada 1: Filtrar por estado y especie
+    // Consulta avanzada 1: Filtro por estado y especie
     List<Animal> findByEstadoAndEspecie_Tipo(Enums.EstadoAnimal estado, Enums.TipoEspecie tipo);
 
-    // Consulta avanzada 2: JPQL para buscar animales de cierta edad en adelante
-    @Query("SELECT a FROM Animal a WHERE a.edad >= ?1")
-    List<Animal> findAnimalesMayoresDe(int edad);
+    // Consulta avanzada 2: JPQL para encontrar animales sin chip
+    @Query("SELECT a FROM Animal a WHERE a.tieneChip = false")
+    List<Animal> findSinChip();
 }
