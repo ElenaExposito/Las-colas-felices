0fbafe917ac827f7d281d4710ab397a4093be844
Antonio <cortes1999@gmail.com>
2026-02-20 13:18:46 +0100

    Resolución Issue #1. Configuración, Enums y Modelos Maestros

---
+++ src/main/java/com/protectora/app/model/Enums.java
@@ -0,0 +1,6 @@
+package com.protectora.app.model;
+
+public class Enums {
+    public enum EstadoAnimal { DISPONIBLE, ADOPTADO, TRATAMIENTO, RESERVADO }
+    public enum TipoEspecie { PERRO, GATO, AVE, REPTIL, OTRO }
+}
